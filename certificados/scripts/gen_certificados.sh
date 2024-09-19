6672cd56a27c40de59db4df979702b5ed445ae39
kreutz <diego.unipampa@gmail.com>
2024-09-19 16:41:08 -0300

    updates

--- certificados/scripts/gen_certificados.sh
+++ certificados/scripts/gen_certificados.sh
@@ -11,7 +11,7 @@ generate_html() {
         AUTORES=$(echo $AUTORES | sed 's/"//g')
         TIPO=$(echo $LINHA | cut -d"," -f1)
         PDF=$(echo $LINHA | cut -d"," -f2)
-        TITULO=$(echo $LINHA | cut -d"," -f3 | sed 's/"//g')
+        TITULO=$(echo $LINHA | cut -d"," -f3)
         echo "<div class=\"slide-example\">"
         echo "<a href=\"$URL/$DIR/$PDF\" target=\"_blank\"><h3>[$TIPO] $TITULO</h3></a>"
         echo "$AUTORES"
@@ -20,8 +20,28 @@ generate_html() {
     done < "$DIR.csv"
 }
 
+generate_html_revisor() {
+    local DIR=$1
+    while IFS=\; read -r LINHA
+    do
+        LINHA=$(echo $LINHA | sed 's/"//g')
+        TIPO=$(echo $LINHA | cut -d"," -f1)
+        PDF=$(echo $LINHA | cut -d"," -f2)
+        TITULO=$(echo $LINHA | cut -d"," -f3)
+        echo "<div class=\"slide-example\">"
+        echo "<a href=\"$URL/$DIR/$PDF\" target=\"_blank\"><h3>[$TIPO] $TITULO</h3></a>"
+        echo "</div>"
+        echo ""
+    done < "$DIR.csv"
+}
+
+
 for i in sf # tp ctdseg wticg
 do
     cat "files/$i.html"
     generate_html "files/$i"
+    if [ -d "files/$i/revisores" ]
+    then
+        generate_html_revisor "files/$i/revisores"
+    fi
 done
